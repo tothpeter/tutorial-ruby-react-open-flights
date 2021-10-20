d48ebd4e46e1f65f9e726a31d640addc19babfcd
Peter Toth <tothpeter08@gmail.com>
2021-10-20 12:20:33 -0500

    Add seed and helper methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,58 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Review.delete_all
+Airline.delete_all
+
+airlines = Airline.create([
+  {
+    name: 'United Airlines',
+    image_url: 'https://open-flights.s3.amazonaws.com/United-Airlines.png'
+  },
+  {
+    name: 'Southwest',
+    image_url: 'https://open-flights.s3.amazonaws.com/Southwest-Airlines.png'
+  },
+  {
+    name: 'Delta',
+    image_url: 'https://open-flights.s3.amazonaws.com/Delta.png'
+  },
+  {
+    name: 'Alaska Airlines',
+    image_url: 'https://open-flights.s3.amazonaws.com/Alaska-Airlines.png'
+  },
+  {
+    name: 'JetBlue',
+    image_url: 'https://open-flights.s3.amazonaws.com/JetBlue.png'
+  },
+  {
+    name: 'American Airlines',
+    image_url: 'https://open-flights.s3.amazonaws.com/American-Airlines.png'
+  }
+])
+
+movies = Review.create(
+  [
+    {
+      title: 'Good',
+      description: 'Good Desc',
+      score: 5.0,
+      airline: airlines.first
+    },
+    {
+      title: 'Bad',
+      description: 'Bad Desc',
+      score: 1.0,
+      airline: airlines.first
+    },
+
+    {
+      title: 'Ok',
+      description: 'Ok Desc',
+      score: 4.0,
+      airline: airlines[1]
+    },
+  ]
+)
