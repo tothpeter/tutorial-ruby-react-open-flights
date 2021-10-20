d98ff96ac26b1015bb66b4c53522554d42112232
Peter Toth <tothpeter08@gmail.com>
2021-10-20 16:44:39 -0500

    Fetch and list the airline names

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
 
 const Airlines = () => {
-  return <div>Airlines index</div>
+  const [airlines, setAirlines] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/v1/airlines')
+    .then( resp => {
+      setAirlines(resp.data.data)
+
+    })
+    .catch( resp => console.log(resp) )
+  }, [])
+
+  const list = airlines.map( item => {
+    return (<li key={item.attributes.name}>{item.attributes.name}</li>)
+  })
+
+  return (
+    <Fragment>
+      <div>Airlines index</div>
+      <ul>{list}</ul>
+    </Fragment>
+  )
 }
 
 export default Airlines
